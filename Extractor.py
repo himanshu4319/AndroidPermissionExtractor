e29363768c91df2baab2d4e1a1b53b8072072509
Himanshu Aggarwal <himanshuaggarwalgarg@gmail.com>
2024-01-18 00:38:52 +0530

    updating Extractor.py
    In this, we tried to include the package name of a particular app to insert on the 0th index and write it to the CSV file along with app permissions.

--- Extractor.py
+++ Extractor.py
@@ -1,7 +1,10 @@
-import os
+import os,shutil,csv
 import subprocess
+import xml.etree.ElementTree as ET
 
 apk_type=0
+permissionCollection = set()
+packageName=''
 def extract_apks(folders):
     for folder in folders:
         apk_type = 0 if folder == './BenignAPK' else 1
@@ -23,13 +26,48 @@ def extract_apks(folders):
             apktool_path=r".\Helper\apktool.bat"
             subprocess.run([apktool_path, "d", "--no-src", "-f", "-s", "-o", f"./UnpackedApk/{name[0]}Extract", target_apk], check=True)
 
-def writerAndRemover(extractPath):
-    
-    
-    with open("data.csv",'w') as file:
-        
-        pass     
+            # specifying unpacked directory or android manifest file path
+            unpackedDir=f"./UnpackedAPK/{name[0]}Extract"
+            manifestPath = unpackedDir+"/AndroidManifest.xml"
+
+            # parsing xml file for fetching permissions
+            try:
+                root = ET.parse(manifestPath).getroot()
+
+                #command for finding package name
+                packageName = root.get("package")
+
+                #command for finding permissions
+                permissions = root.findall("uses-permission")
+                # print(permissions)
+
+                print("  SET STATUS :", end=' ')        # ADD NEW PERMISSION TO THE LIST
+                for perm in permissions:
+                    for att in perm.attrib:
+                        permelement = perm.attrib[att]
+                        if permelement in permissionCollection:
+                            print("0", end=' ')
+                        else:
+                            # print("1", end=' ')
+                            permissionCollection.add(permelement)
+            except:
+                print("An exception is encountered")
+
+            shutil.rmtree(unpackedDir)
+            # print(permissionCollection)
+            permList = list(permissionCollection)
+
+            permList.insert(0,packageName)
+
+            with open(f"UpdatePermList.csv", 'a') as file:        # SAVE LIST IN FILE.
+                writer = csv.DictWriter(file,fieldnames=permList)
+                # writer.writeheader()
+                for i in permList:
+                    file.write(f"{i},")
+                file.write("\n\n")                    
+
+  
             
 if __name__ == "__main__":
-    apk_folders = ['./MalwareAPK', './BenignAPK']
+    apk_folders = ['./BenignAPK','./MalwareAPK',]
     extract_apks(apk_folders)
